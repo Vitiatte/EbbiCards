f83a3f1a0351f166508ab5d3b7a57cfb984de1de
Vitiatte <131995root>
2020-01-19 16:11:51 +0200

    Init commit

---
+++ src/main/java/com/ebbi/EbbiCards/EbbiCardsApplication.java
@@ -0,0 +1,13 @@
+package com.ebbi.EbbiCards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbbiCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbbiCardsApplication.class, args);
+	}
+
+}
